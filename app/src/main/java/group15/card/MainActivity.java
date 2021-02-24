3f7bb697f601d94cde34fd48cfd4d1a6c3f440ce
NikoStein96 <nikolai_stein@hotmail.com>
2021-02-24 12:44:19 +0100

    refactoring, mvp næsten done

--- app/src/main/java/group15/card/MainActivity.java
+++ app/src/main/java/group15/card/MainActivity.java
@@ -6,8 +6,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
-
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -17,29 +17,18 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
 import android.provider.MediaStore;
-import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import org.json.JSONException;
-import org.json.JSONObject;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 
 import okhttp3.MediaType;
-import okhttp3.MultipartBody;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
 
-import static group15.card.TestPostActivity.MEDIA_TYPE_MARKDOWN;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     ImageView kabale_imageview;
     Button takeImgButton, nextMoveButton;
@@ -49,27 +38,39 @@ public class MainActivity extends AppCompatActivity {
     private TextView popup_header_text, popup_instructions_text;
     private Button popup_close_button;
 
+    final LoadingDialog loadingDialog = new LoadingDialog(MainActivity.this);
+
     private File photoFile;
     private String FILE_NAME = "photo.jpg";
-
-    public static final MediaType MEDIA_TYPE_MARKDOWN
-            = MediaType.parse("text/x-markdown; charset=utf-8");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-   //     Intent i = new Intent(this, TestPostActivity.class);
-     //   startActivity(i);
-
-        //loading
-        final LoadingDialog loadingDialog = new LoadingDialog(MainActivity.this);
-
-
         kabale_imageview = findViewById(R.id.kabale_imageview);
         takeImgButton = findViewById(R.id.takeImgButton);
 
+        // Get camera permission
+        getCameraPermission();
+
+        takeImgButton.setOnClickListener(this);
+
+        nextMoveButton = findViewById(R.id.nextMoveButton);
+        nextMoveButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == takeImgButton){
+           openCameraApp();
+        }
+        if(v == nextMoveButton){
+            createNewMoveDialog();
+        }
+    }
+
+    private void getCameraPermission(){
         if (ContextCompat.checkSelfPermission(MainActivity.this,
                 Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(MainActivity.this,
@@ -78,72 +79,25 @@ public class MainActivity extends AppCompatActivity {
                     },
                     100);
         }
+    }
 
-        takeImgButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                try {
-                    photoFile = getPhotoFile(FILE_NAME);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                // muligvis fejl her
-                Uri fileProvider = FileProvider.getUriForFile(getApplicationContext(), "group15.card.fileprovider", photoFile);
-                intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
-
-
-                startActivityForResult(intent, 100);
-
-                loadingDialog.startLoadingDialog();
-                Handler handler = new Handler();
-                handler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        loadingDialog.dismissDialog();
-                    }
-                }, 5000);
-
-            }
-        });
-
-        nextMoveButton = findViewById(R.id.nextMoveButton);
-        nextMoveButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                createNewMoveDialog();
-            }
-        });
-
-        /*
-              if(!hasCameraPermission()) {
-            requestCameraPermission();
+    private void openCameraApp(){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try {
+            photoFile = getPhotoFile(FILE_NAME);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        */
+
+        Uri fileProvider = FileProvider.getUriForFile(this, "group15.card.fileprovider", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+        startActivityForResult(intent, 100);
     }
 
     private File getPhotoFile(String fileName) throws IOException {
         File storageDirectory = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
         return File.createTempFile(fileName,"jpg",storageDirectory);
     }
-
-    private boolean hasCameraPermission() {
-        return ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
-    }
-
-    private void requestCameraPermission() {
-        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, 1);
-    }
-
-    /*
-    @Override
-    public void onClick(View view) {
-        if (view == takeImgButton) {
-            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            startActivityForResult(intent,100);
-        }
-    }
-     */
 
     public void createNewMoveDialog() {
         dialogBuilder = new AlertDialog.Builder(this);
@@ -162,24 +116,19 @@ public class MainActivity extends AppCompatActivity {
                 dialog.dismiss();
             }
         });
-
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         if (requestCode == 100) {
-          //  Bitmap captureImage = (Bitmap) data.getExtras().get("data")
-
-
             Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
             kabale_imageview.setImageBitmap(takenImage);
 
-            new SendImageTask().execute(photoFile.getAbsolutePath());
-
+            new SendImageTask(MainActivity.this).execute(photoFile.getAbsolutePath());
         }
     }
 
-
-
-
+    public void setMessage(String message){
+        popup_instructions_text.setText(message);
+    }
 }
--- app/src/main/java/group15/card/SendImageTask.java
+++ app/src/main/java/group15/card/SendImageTask.java
@@ -2,8 +2,7 @@ package group15.card;
 
 import android.os.AsyncTask;
 
-import org.json.JSONException;
-import org.json.JSONObject;
+import androidx.appcompat.app.AlertDialog;
 
 import java.io.File;
 import java.io.IOException;
@@ -17,6 +16,22 @@ import okhttp3.Response;
 
 public class SendImageTask extends AsyncTask<String, Void, String> {
 
+    private AlertDialog.Builder dialogBuilder;
+    private AlertDialog dialog;
+
+    private MainActivity activity;
+    final LoadingDialog loadingDialog;
+
+    Response response;
+
+    public SendImageTask(MainActivity activity) {  // can take other params if needed
+        this.loadingDialog = new LoadingDialog(activity);
+    }
+
+    @Override
+    protected void onPreExecute() {
+        loadingDialog.startLoadingDialog();
+    }
 
     @Override
     protected String doInBackground(String... strings) {
@@ -31,20 +46,19 @@ public class SendImageTask extends AsyncTask<String, Void, String> {
                 .url("http://192.168.1.40:5005/upload")
                 .post(requestBody)
                 .build();
-        Response response = null;
+        response = null;
         try {
             response = okHttpClient.newCall(request).execute();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        try {
-            JSONObject jsonObject = new JSONObject(response.body().string());
-            return jsonObject.optString("image");
-        } catch (JSONException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return "";
+        System.out.println(response.message());
+        return response.message();
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        loadingDialog.dismissDialog();
+        //activity.setMessage(response.message().toString());
     }
 }
